ddba5542488945e31352b05ed5f09808215e2d4b
Reuben Jones <mrreubenjones@gmail.com>
2016-11-14 15:43:43 -0800

    Add user sign up and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   root 'posts#index'
   get '/about' => 'home#about'
 
-  resources :posts
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
+  resources :posts, shallow: true do
+    resources :comments, only: [:create, :destroy]
+  end
 
 end
