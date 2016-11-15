70fa07ac7460be4e26be8fcb584ef0cc8bf5bef6
Reuben Jones <mrreubenjones@gmail.com>
2016-11-14 20:09:59 -0800

    Adding categories to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115012216) do
+ActiveRecord::Schema.define(version: 20161115040354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,11 @@ ActiveRecord::Schema.define(version: 20161115012216) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
@@ -59,5 +61,6 @@ ActiveRecord::Schema.define(version: 20161115012216) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
 end
